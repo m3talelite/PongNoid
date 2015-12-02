d501dc693ecf5d095f40c0cf67e6c8bc5dcaa85d
Mitchell Slager <mslager92@gmail.com>
2015-12-02 14:56:43 +0100

    Added folders and more setup to scene

---
+++ Assets/Paddle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	public float paddleSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
