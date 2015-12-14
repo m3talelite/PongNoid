a6d05f905f519d6536a32e44aabedb95c42309fc
m3talelite <Jelle@Braat.NU>
2015-12-14 15:12:08 +0100

    started on game logic

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Block : MonoBehaviour {
+    protected int toughness;
+    protected bool hasSpecial;
+
+    public void DecreaseToughness()
+    {
+        if (toughness <= 0)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            --toughness;
+        }
+    }
+
+    public abstract void OnCollision();
+}
