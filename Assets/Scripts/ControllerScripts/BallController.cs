524aac698e26ca7e99167522e22428420d3deb01
m3talelite <Jelle@Braat.NU>
2016-01-09 21:15:17 +0100

    keep the balls centered, launch direction of ball added

--- Assets/Scripts/ControllerScripts/BallController.cs
+++ Assets/Scripts/ControllerScripts/BallController.cs
@@ -13,6 +13,13 @@ public class BallController : MonoBehaviour {
 
     void Start()
     {
+        switch (PlayerPrefs.GetInt("Difficulty")) {
+            case 0: speed = 200.0f; break;
+            case 1: speed = 200.0f; break;
+            case 2: speed = 250.0f; break;
+            default: speed = 200.0f; break;
+        }
+
         direction = owner.tag.Equals("PaddlePlayerOne") ? 1.0f : -1.0f;
         body = GetComponent<Rigidbody>();
     }
@@ -35,13 +42,30 @@ public class BallController : MonoBehaviour {
         {
             MoveBallOnPaddle();
         }
+        //TODO::Remove this later!
+        if (Input.GetButtonDown("Test"))
+        {
+            body.AddForce(0,0,1.0f);
+            Debug.Log("Added test force");
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag.Equals("PaddlePlayerOne") || collision.gameObject.tag.Equals("PaddlePlayerTwo"))
         {
+            //Todo check if works maybe replace?
+            GameObject paddle = collision.gameObject;
+            Vector3 v = body.velocity;
+            v.Normalize();
+            float dotOfvn = Vector3.Dot(v, (direction == 1.0f ? Vector3.left : Vector3.right));
+
+            Vector3 R = new Vector3();
+            R += -2 * dotOfvn * (direction == 1.0f ? Vector3.left : Vector3.right);
+            //TODO: finish method 
             Debug.Log("Ball hit paddle");
+
+
         }
         if (collision.gameObject.tag.Equals("Block"))
         {
@@ -57,7 +81,10 @@ public class BallController : MonoBehaviour {
         *   coef = (ball.y - paddle.y) / (paddle.height * 0.5) // this will give you a number between -1 (top) and 1 (bottom)
         *   ball.speedY  = MAX_Y_SPEED * coef
         */
-        body.AddForce(speed * direction, speed, 0);
+        float maxSpeedY = 150.0f;
+        float coeff = (transform.localPosition.y - owner.transform.localPosition.y) / (owner.transform.localScale.y * 0.5f);
+        Debug.Log("Logging start coeff: " + coeff); 
+        body.AddForce(speed * direction, maxSpeedY * coeff, 0);
     }
 
     void MoveBallOnPaddle()
--- Assets/Scripts/ControllerScripts/PaddleController.cs
+++ Assets/Scripts/ControllerScripts/PaddleController.cs
@@ -10,33 +10,22 @@ public class PaddleController : MonoBehaviour {
 	void Start()
     {
         playerPosition = gameObject.tag.Equals("PaddlePlayerOne") ? new Vector3(-6.75f, 0, 0) : new Vector3(6.75f, 0, 0);
-        switch (PlayerPrefs.GetInt("Difficulty"))
-        {
-            case 0:
-                transform.localScale = new Vector3(0.25f, 4.0f, 0.5f);
-                break;
-            case 1:
-                transform.localScale = new Vector3(0.25f, 2.5f, 0.5f);
-                break;
-            case 2:
-                transform.localScale = new Vector3(0.25f, 1.5f, 0.5f);
-                break;
-            default:
-                transform.localScale = new Vector3(0.25f, 4.0f, 0.5f);
-                break;
+        switch (PlayerPrefs.GetInt("Difficulty")) { 
+            case 0: transform.localScale = new Vector3(0.25f, 4.0f, 0.5f); paddleSpeed = 0.1f; break;
+            case 1: transform.localScale = new Vector3(0.25f, 2.5f, 0.5f); paddleSpeed = 0.12f; break;
+            case 2: transform.localScale = new Vector3(0.25f, 1.5f, 0.5f); paddleSpeed = 0.14f; break;
+            default: transform.localScale = new Vector3(0.25f, 4.0f, 0.5f); paddleSpeed = 0.1f; break;
         }
     }
 
     void FixedUpdate()
     {
-        if (gameObject.tag.Equals("PaddlePlayerOne"))
-        {
+        if (gameObject.tag.Equals("PaddlePlayerOne")) {
             float posY = transform.position.y + (Input.GetAxis("Vertical") * paddleSpeed);
             playerPosition = new Vector3(playerPosition.x, Mathf.Clamp(posY, -5 + (transform.localScale.y / 2), 5f - (transform.localScale.y / 2)), playerPosition.z);
             transform.position = playerPosition;
         } 
-        else
-        {
+        else {
             float posY = transform.position.y + (Input.GetAxis("VerticalTwo") * paddleSpeed);
             playerPosition = new Vector3(playerPosition.x, Mathf.Clamp(posY, -5 + (transform.localScale.y / 2), 5f - (transform.localScale.y / 2)), playerPosition.z);
             transform.position = playerPosition;
